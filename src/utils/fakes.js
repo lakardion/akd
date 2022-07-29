5d1faf8a43964104c4744028590a97669f0ce8d9
lakardion <lakardion@protonmail.com>
2022-07-29 00:47:22 -0300

    dx 💻 add faker

---
+++ src/utils/fakes.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker'
+
+export const getFakeStudents = (count) => {
+  return [...Array(count).keys()].map(arr => ({
+    name: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    university: faker.company.companyName(),
+    faculty: faker.vehicle.manufacturer(),
+    course: faker.vehicle.fuel()
+  }))
+}
