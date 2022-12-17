98dc900df1358fc48dcb0bd363b7a9d646a729b0
lakardion <lakardion@protonmail.com>
2022-12-17 18:13:49 -0300

    feat(dashboard) add some placeholders

--- src/components/dashboard/new-students-card.tsx
+++ src/components/dashboard/new-students-card.tsx
@@ -1,7 +1,23 @@
+import { Spinner } from 'components/spinner';
 import { trpc } from 'utils/trpc';
 
 export const NewStudentsCard = () => {
-  const { data: newStudents } = trpc.analytics.newStudents.useQuery();
+  const { data: newStudents, isLoading } =
+    trpc.analytics.newStudents.useQuery();
+  if (isLoading) {
+    return (
+      <section className="flex h-full w-full items-center justify-center">
+        <Spinner size="md" />
+      </section>
+    );
+  }
+  if (!newStudents?.length) {
+    return (
+      <section className="flex w-full justify-center">
+        <p className="italic">No hubo nuevos estudiantes este mes</p>
+      </section>
+    );
+  }
   return (
     <section className="flex w-full flex-col gap-3">
       <div className="flex items-center justify-center gap-2">
